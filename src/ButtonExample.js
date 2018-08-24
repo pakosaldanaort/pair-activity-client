787d3aaae9045c666654f13f28d48e9f7af0d288
marcbernal <marcbernal@gmail.com>
2018-08-24 16:44:48 -0500

    Adedd button component

---
+++ src/ButtonExample.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class ButtonExample extends React.Component {
+    render() {
+        return (
+            <button>Example</button>
+        )
+    }
+}
+
+export default ButtonExample;
